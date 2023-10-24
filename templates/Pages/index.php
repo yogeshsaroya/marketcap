ccb38b2858a36011e556567c5301b2e23f1d72e7
Yogesh Saroya <yogeshsaroya@gmail.com>
2023-10-24 12:58:57 +0530

    a

--- templates/Pages/index.php
+++ templates/Pages/index.php
@@ -140,8 +140,8 @@ $cap = $this->Data->getCaps();
             <?php
             if($this->request->is('mobile')){
                 echo $this->Paginator->first(__('First', true), array('tag' => 'li', 'escape' => false), array('type' => "button", 'class' => "page-link"));
-                echo $this->Paginator->prev('&laquo;', array('tag' => 'li', 'escape' => false), '<a href="#">&laquo;</a>', array('class' => 'prev disabled', 'tag' => 'li', 'escape' => false));
-                echo $this->Paginator->next('&raquo;', array('tag' => 'li', 'escape' => false), '<a href="#">&raquo;</a>', array('class' => 'prev disabled', 'tag' => 'li', 'escape' => false));
+                echo $this->Paginator->prev('Next', array('tag' => 'li', 'escape' => false), '<a href="#">&laquo;</a>', array('class' => 'prev disabled', 'tag' => 'li', 'escape' => false));
+                echo $this->Paginator->next('Pre', array('tag' => 'li', 'escape' => false), '<a href="#">&raquo;</a>', array('class' => 'prev disabled', 'tag' => 'li', 'escape' => false));
                 echo $this->Paginator->last(__('Last', true), array('tag' => 'li', 'escape' => false), array('type' => "button", 'class' => "page-link"));
 
             }else{
